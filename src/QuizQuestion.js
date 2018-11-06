0407cc83a1b865abedeab7e12dd8bcd18568ea2d
Kale Roberts <kroberts@netspend.com>
2018-11-06 16:13:47 -0600

    Display error message when selecting an incorrect message.

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,10 +3,21 @@ import { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      incorrectAnswer: false
+    }
+  }
 
   handleClick(buttonText) {
     if(buttonText === this.props.quiz_question.answer) {
-      this.props.showNextQuestionHandler()
+      this.props.showNextQuestionHandler();
+      this.setState(prevState => {
+        return {incorrectAnswer: false}
+      })
+    } else {
+      this.setState( {incorrectAnswer: true} )
     }
   }
 
@@ -29,6 +40,7 @@ class QuizQuestion extends Component {
             {/*<QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>*/}
           </ul>
         </section>
+        {this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null}
       </main>
     )
   }
